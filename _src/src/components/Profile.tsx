1feed08f0519959fcef6f1c2f461038ce16ed1be
Vitor Martins <vitormartinsweb@gmail.com>
2021-02-28 14:06:06 +0000

    UPD finished nlw - sent to production

--- _src/src/components/Profile.tsx
+++ _src/src/components/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
 	const {level} = useContext(ChallengesContext)
 	return (
 		<div className={styles.profileContainer}>
-			<img src="https://i.pravatar.cc/300" alt=""/>
+			<img src="https://avatars.githubusercontent.com/u/37670851?s=460&u=23a41cf27b38ee6a6efad71cc2440bde915e3d2a&v=4" alt="avatar"/>
 			<div>
 				<strong>Vitor Martins</strong>
 				<p>
